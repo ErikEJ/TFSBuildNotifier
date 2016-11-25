45a7e77738d33d8fbe3798cc7df9a0ee602fb617
ErikEJ <ejlskov@hotmail.com>
2016-11-25 10:36:36 +0100

    Now works with TFS on-prem!

--- src/TFSBuildNotifier/TfsBuildStatusProvider/TfsStatusProvider.cs
+++ src/TFSBuildNotifier/TfsBuildStatusProvider/TfsStatusProvider.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.Http;
-using System.Runtime.Serialization.Json;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TFSBuildNotifier.TfsBuildStatusProvider
 {
@@ -14,57 +14,52 @@ namespace TFSBuildNotifier.TfsBuildStatusProvider
             var result = new List<BuildStatus>();
             foreach (var uri in uriList)
             {
-                var buildStatus = new BuildStatus();
-                buildStatus.Key = uri;
-                buildStatus.BuildId = 0;
-                buildStatus.Status = Status.Undetermined;
-                buildStatus.BuildName = "N/A";
-                buildStatus.RequestedBy = "Unknown";
+                var buildStatus = new BuildStatus
+                {
+                    Key = uri,
+                    BuildId = 0,
+                    Status = Status.Undetermined,
+                    BuildName = "N/A",
+                    RequestedBy = "Unknown"
+                };
                 try
                 {
-                    var body = GetJsonPayloadAsync(uri).GetAwaiter().GetResult();
-                    var serializer = new DataContractJsonSerializer(typeof(Rootobject));
-                    using (var stream = GenerateStreamFromString(body))
+                    var body = GetJsonPayload(uri);
+                    var response = JsonConvert.DeserializeObject<Rootobject>(body);
+
+                    buildStatus.Link = new Uri(response.value[0]._links.web.href);
+                    buildStatus.BuildName = response.value[0].definition.name;
+                    buildStatus.RequestedBy = response.value[0].requestedBy.displayName;
+                    buildStatus.Status = Status.Success;
+
+                    var status = response.value[0].result;                    
+                    if (status != "succeeded")
                     {
-                        var response = (Rootobject)serializer.ReadObject(stream);
-                        buildStatus.Link = new Uri(response.value[0]._links.web.href);
-                        buildStatus.BuildName = response.value[0].definition.name;
-                        buildStatus.RequestedBy = response.value[0].requestedBy.displayName;
-                        var status = response.value[0].result;
-                        buildStatus.Status = Status.Success;
-                        if (status != "succeeded")
-                        {
-                            buildStatus.Status = Status.Error;
-                        }
-                        //TODO Do more granular status?
-                        //https://www.visualstudio.com/en-us/docs/integrate/api/build/builds
+                        buildStatus.Status = Status.Error;
                     }
+                    //TODO Do more granular status?
+                    //https://www.visualstudio.com/en-us/docs/integrate/api/build/builds
                 }
-                catch
+                catch (Exception ex)
                 {
-                    //Ignored                    
+                    File.WriteAllText(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()), ex.ToString());
                 }
                 result.Add(buildStatus);
             }
             return result;
         }
 
-        private static Stream GenerateStreamFromString(string s)
+        private string GetJsonPayload(Uri url)
         {
-            var stream = new MemoryStream();
-            var writer = new StreamWriter(stream);
-            writer.Write(s);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
-
-        private async Task<string> GetJsonPayloadAsync(Uri url)
-        {
-            var client = new HttpClient();
-            var response = await client.GetAsync(url);
+            var client = new HttpClient(new HttpClientHandler
+            {
+                Credentials = CredentialCache.DefaultNetworkCredentials,
+                UseDefaultCredentials = true
+            });
+            client.Timeout = new TimeSpan(0,0, 10);
+            var response = client.GetAsync(url).Result;
             response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsStringAsync();
+            return response.Content.ReadAsStringAsync().Result;
         }
     }
 }
---
+++ src/TFSBuildNotifier/readme.txt
@@ -0,0 +1,3 @@
+﻿Sample API Url:
+
+http://<host>/<Collection>/<project>/_apis/build/builds?definitions=865&$top=1&api-version=2.0&statusFilter=completed
