651614baf2a13428d2b6be454014979ab7306b21
Erik Ejlskov Jensen <Erik Ejlskov Jensen>
2016-11-27 14:48:41 +0100

    Remove obsolete line

--- tools/ILMerge.cmd
+++ tools/ILMerge.cmd
@@ -1,4 +1,2 @@
 echo Running ilmerge...
-rem .\ilmerge /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:..\TfsBuildNotifier.exe ..\src\TfsBuildNotifier\bin\release\TfsBuildNotifier.exe ..\src\TfsBuildNotifier\bin\release\Hardcodet.Wpf.TaskbarNotification.dll
-
 .\src\TFSBuildNotifier\packages\ILMerge.2.14.1208\tools\ilmerge /out:TfsBuildNotifier.exe src\TfsBuildNotifier\bin\release\TfsBuildNotifier.exe src\TfsBuildNotifier\bin\release\Newtonsoft.Json.dll src\TfsBuildNotifier\bin\release\Hardcodet.Wpf.TaskbarNotification.dll
